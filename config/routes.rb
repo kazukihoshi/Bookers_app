e22e3fc78cca0ef91516698a58afc5e0bd71d3a5
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-06-13 13:58:46 +0000

    [add]indexファイル記載終了

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  post 'books'=>'books#create'
+  get 'books'=>'books#index'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/top'=> 'homes#top'
+end
