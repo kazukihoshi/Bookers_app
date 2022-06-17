4c58b22836dfe6e7f359dd85dfbf79e52e835128
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-06-17 13:27:23 +0000

    [add]creatでのエラー

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(book.id)
+    else
+      render :index
+    end
   end
 
   def index
@@ -34,6 +37,7 @@ class BooksController < ApplicationController
   end
   
   private
+  
   def book_params
     params.require(:book).permit(:title,:body)
   end
